0754dd6338f803a6fed033d44b7a7d356d6a6472
Benoist <benoist.claassen@gmail.com>
2013-09-09 13:10:56 +0200

    fixed rake test

--- Rakefile
+++ Rakefile
@@ -3,6 +3,7 @@ require 'rake'
 require 'rake/testtask'
 
 Rake::TestTask.new do |t|
-  t.pattern = 'test/**/*_test.rb'
+  t.libs.push 'lib'
   t.libs.push 'test'
+  t.pattern = 'test/**/*_test.rb'
 end
--- test/all.rb
+++
@@ -1,9 +0,0 @@
-test = File.expand_path('../test', __FILE__)
-$LOAD_PATH.unshift(test) unless $LOAD_PATH.include?(test)
-$: << 'test'
-
-require 'test_helper'
-
-Dir['./**/*_test.rb'].each do |test_file|
-  require test_file
-end
